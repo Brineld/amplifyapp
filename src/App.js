9d1142d2e01bd104500a7b0f543905c490bcb49f
Brinel D'souza <48150561+Brineld@users.noreply.github.com>
2020-04-04 15:10:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,13 +16,21 @@ class App extends Component {
     .then(json=>this.setState({posts:json}))
   }
  render(){
+   const { posts } = this.state;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!
-        </p>
-      </header>
+              <div class="jumbotron">
+          <h1 class="display-4">Blog posts</h1>
+        </div>
+    {posts.map((post) => (
+        <div className="card" key={post.id}>
+          <div className="card-header">
+            #{post.id} {post.title}
+          </div>
+          <div className="card-body">            
+            <p className="card-text">{post.body}</p>
+          </div>
+        </div>
     </div>
   );
 }
